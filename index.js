980f7033c93c9cb85cefa56025a09d6df686b85b
Spencer D Stone <doomydoomdomm@gmail.com>
2018-09-29 05:53:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   return kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop
+}
